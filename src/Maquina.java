365b16f81451852fe4a716f969b6cfb565440c41
deivbbmd <xdxrovied@gmail.com>
2018-04-19 14:51:30 +0000

    Mejorado tarea

--- src/Maquina.java
+++ src/Maquina.java
@@ -3,14 +3,21 @@ import java.util.Vector;
 
 public class Maquina {
 	
+	//vector en el que se se almacenan las tareas en el orden de ejecución
 	private Vector<Tarea> tareasrealizadas;
 	
 	
 	
 	
 	
-	
-
+	public Integer getTimeTotal(){
+		Integer aux = 0;
+		for (int i = 0; i < tareasrealizadas.size();i++ ){
+			aux += tareasrealizadas[i].getTime();
+		}
+	return aux;
+		
+	}
 	public Vector<Tarea> getTareasrealizadas() {
 		return tareasrealizadas;
 	}
