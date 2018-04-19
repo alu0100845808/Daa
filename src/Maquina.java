8b45915dbf54633bdff0e102102160150e13f748
deivbbmd <xdxrovied@gmail.com>
2018-04-19 14:41:52 +0000

    Empezando

---
+++ src/Maquina.java
@@ -0,0 +1,25 @@
+import java.util.Vector;
+
+
+public class Maquina {
+	
+	private Vector<Tarea> tareasrealizadas;
+	
+	
+	
+	
+	
+	
+
+	public Vector<Tarea> getTareasrealizadas() {
+		return tareasrealizadas;
+	}
+
+	public void setTareasrealizadas(Vector<Tarea> tareasrealizadas) {
+		this.tareasrealizadas = tareasrealizadas;
+	}
+
+	
+
+
+}
---
+++ src/Tarea.java
@@ -0,0 +1,13 @@
+
+public class Tarea {
+	private Integer time;
+
+	public Integer getTime() {
+		return time;
+	}
+
+	public void setTime(Integer time) {
+		this.time = time;
+	}
+	
+}
